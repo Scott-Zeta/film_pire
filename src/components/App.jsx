7de5a8553b1ac7337168b74a13970a4e354dd680
Scott-Zeta <wc070211@gmail.com>
2023-03-29 18:55:27 +1030

    import CSSbasline

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { CssBaseline } from '@mui/material';
 
-const App = () => <div><h1>Test</h1></div>;
+const App = () => (
+  <div>
+    <CssBaseline />
+    <h1>Test</h1>
+  </div>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// eslint-disable-next-line import/no-named-as-default, import/no-named-as-default-member
 import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
