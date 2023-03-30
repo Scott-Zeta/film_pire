515b51381ce55abfb0d0dcbf38705545a4e544ec
Scott-Zeta <wc070211@gmail.com>
2023-03-30 17:50:01 +1030

    responsive toolbar by mui

--- src/components/NavBar/navBar.jsx
+++ src/components/NavBar/navBar.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
-import { AppBar, IconButton, Toolbar, Drawer, Button, Avatar } from '@mui/material';
+import { AppBar, IconButton, Toolbar, Drawer, Button, Avatar, useMediaQuery } from '@mui/material';
 import { Menu, AccountCircle, Brightness4, Brightness7 } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
 import useStyles from './styles';
 
 const NavBar = () => {
   const classes = useStyles();
+  const isMobile = useMediaQuery('(max-width:600px)');
   return (
     <>
       <AppBar position="fixed">
         <Toolbar className={classes.toolbar}>
-          <button>Button 1</button>
-          <button>Button 2</button>
-          <button>Button 3</button>
+          {isMobile && (
+            <IconButton
+              color="inherit"
+              edge="start"
+              style={{ outline: 'none' }}
+              onClick={() => {}}
+              className={classes.menuButton}
+            >
+              <Menu />
+            </IconButton>
+          )}
         </Toolbar>
       </AppBar>
     </>
