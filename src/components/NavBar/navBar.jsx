0f4b5ee65a1551e7b90902cd5c511618749fb086
Scott-Zeta <wc070211@gmail.com>
2023-03-30 17:22:42 +1030

    set the tool bar

--- src/components/NavBar/navBar.jsx
+++ src/components/NavBar/navBar.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { AppBar, IconButton, Toolbar, Drawer, Button, Avatar } from '@mui/material';
+import { Menu, AccountCircle, Brightness4, Brightness7 } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+import useStyles from './styles';
 
-const NavBar = () => (
-  <div>
-    <h1>NavBar</h1>
-  </div>
-);
+const NavBar = () => {
+  const classes = useStyles();
+  return (
+    <>
+      <AppBar position="fixed">
+        <Toolbar className={classes.toolbar}>
+          <button>Button 1</button>
+          <button>Button 2</button>
+          <button>Button 3</button>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
 
 export default NavBar;
