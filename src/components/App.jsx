34d6bea9928d1bbafaea2a64b3f2ca4f4a2e50e8
Scott-Zeta <wc070211@gmail.com>
2023-03-30 13:50:28 +1030

    Router setting

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import { CssBaseline } from '@mui/material';
+import { Route, Switch } from 'react-router-dom';
 
 const App = () => (
   <div>
     <CssBaseline />
-    <h1>Test</h1>
+    <main>
+      <Switch>
+        <Route exact path="/">
+          <h1>Home</h1>
+        </Route>
+        <Route exact path="/movie/:id">
+          <h1>Movie Info</h1>
+        </Route>
+        <Route exact path="/actors/:id">
+          <h1>Actor Info</h1>
+        </Route>
+        <Route exact path="/profile/:id">
+          <h1>Profile Info</h1>
+        </Route>
+      </Switch>
+    </main>
   </div>
 );
 
