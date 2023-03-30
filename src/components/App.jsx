ddd31a5f1b3cdd6eeb65a4ba1607c7bb493e04d0
Scott-Zeta <wc070211@gmail.com>
2023-03-30 15:12:54 +1030

    access the mui styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,28 +2,32 @@ import React from 'react';
 import { CssBaseline } from '@mui/material';
 import { Route, Switch } from 'react-router-dom';
 import { Actors, Movies, MovieInfomation, NavBar, Profile } from './ComponentsIndex';
+import useStyles from './styles';
 
-const App = () => (
-  <div>
-    <CssBaseline />
-    <NavBar />
-    <main>
-      <Switch>
-        <Route exact path="/">
-          <Movies />
-        </Route>
-        <Route exact path="/movie/:id">
-          <MovieInfomation />
-        </Route>
-        <Route exact path="/actors/:id">
-          <Actors />
-        </Route>
-        <Route exact path="/profile/:id">
-          <Profile />
-        </Route>
-      </Switch>
-    </main>
-  </div>
-);
+const App = () => {
+  const classes = useStyles();
+  return (
+    <div>
+      <CssBaseline />
+      <NavBar />
+      <main>
+        <Switch>
+          <Route exact path="/">
+            <Movies />
+          </Route>
+          <Route exact path="/movie/:id">
+            <MovieInfomation />
+          </Route>
+          <Route exact path="/actors/:id">
+            <Actors />
+          </Route>
+          <Route exact path="/profile/:id">
+            <Profile />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
+};
 
 export default App;
