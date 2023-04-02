09debecb82426d5b0d8768c0c7ddf2285da94edb
Scott-Zeta <wc070211@gmail.com>
2023-04-02 22:07:50 +0930

    Case sensitive?

--- src/components/NavBar/navBar.jsx
+++ src/components/NavBar/navBar.jsx
@@ -24,7 +24,7 @@ const NavBar = () => {
   const isMobile = useMediaQuery('(max-width:600px)');
   const theme = useTheme();
   // dummy variable for test
-  const isAuthenticated = false;
+  const isAuthenticated = true;
 
   return (
     <>
