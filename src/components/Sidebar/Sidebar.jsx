5f9c5444f88a4ca98b58f2ac89c9040715bb09f7
scott-zeta <wc070211@gmail.com>
2023-04-04 15:19:58 +0930

    Sidebar styles

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,14 +2,15 @@ import React, { useEffect } from 'react';
 import { Divider, List, ListItem, ListItemText, ListSubheader, ListItemIcon, Box, CircularProgress } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { useTheme } from '@emotion/react';
+import useStyles from './styles';
 
 const Sidebar = ({ setMobileOpen }) => {
   const theme = useTheme();
-
+  const classes = useStyles();
   return (
-    <div>
-      Sidebar
-    </div>
+    <>
+      <Link to="/" className={classes.imageLink} />
+    </>
   );
 };
 
