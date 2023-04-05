00cbb0171cab2f971652bb60f385f280ef6bd894
scott-zeta <wc070211@gmail.com>
2023-04-05 17:34:24 +0930

    set api

---
+++ src/services/TMDB.js
@@ -0,0 +1,16 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const tmbdApiKey = process.env.TMDB_KEY;
+const page = 1;
+
+export const tmbdApi = createApi({
+  reducerPath: 'tmdbApi',
+  // this is for app's api call
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://api.themoviedb.org/3' }),
+  endpoints: (builder) => ({
+    // Get movies by [Type]
+    getMoivies: builder.query({
+      query: () => `movie/popular?page=${page}&api_key=${tmbdApiKey}`,
+    }),
+  }),
+});
