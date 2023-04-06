05a6e65207efc33522641e41a2e316157b1e3ea7
scott-zeta <wc070211@gmail.com>
2023-04-06 20:54:08 +0930

    Growing elements

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -3,14 +3,17 @@ import { Typography, Grid, Grow, Tooltip, Rating } from '@mui/material';
 import { Link } from 'react-router-dom';
 import useStyles from './styles';
 
-const Movie = ({ movie }) => {
+const Movie = ({ movie, i }) => {
   const classes = useStyles();
   console.log(movie);
   return (
     <Grid item xs={12} sm={6} md={3} lg={4} xl={2} className={classes.movie}>
-      <Typography className={classes.title} variant="h5">
-        {movie.title}
-      </Typography>
+      {/* Grow is a loading animation component */}
+      <Grow in key={i} timeout={250}>
+        <Typography className={classes.title} variant="h5">
+          {movie.title}
+        </Typography>
+      </Grow>
     </Grid>
   );
 };
